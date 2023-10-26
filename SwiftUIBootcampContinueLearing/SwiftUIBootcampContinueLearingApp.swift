d2ab519efdcb1257e55a8c254e4347be398cd0be
Dilip <dilip.tiwari333@gmail.com>
2023-10-26 14:40:17 +0530

    Initial Commit

---
+++ SwiftUIBootcampContinueLearing/SwiftUIBootcampContinueLearingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampContinueLearingApp.swift
+//  SwiftUIBootcampContinueLearing
+//
+//  Created by Dilip Tiwari on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampContinueLearingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
